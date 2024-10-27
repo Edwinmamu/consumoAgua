ac6ccdd39960c265913fc4a1b32b0b452780898a
Edwin <lgedwin.martinez@gmail.com>
2024-10-27 14:20:20 -0500

    inicio de ejercicio, se implementa el inicio de sesion

---
+++ Ejercicio.js
@@ -0,0 +1,33 @@
+
+// Valores predefinidos para el usuario y contraseña
+const usuarioPredefinido = "admin";
+const contraseñaPredifinida = "1234";
+
+// Función para  el inicio de sesión
+function iniciarSesion() {
+    let intentos = 0;
+
+    while (intentos < 3) {
+        const usuario = prompt("Ingrese su nombre de usuario:");
+        const contrasena = prompt("Ingrese su contraseña:");
+
+        if (usuario === usuarioPredefinido && contrasena === contraseñaPredifinida) {
+
+            alert('Bienvenido' + ' ' + usuario);
+            return true; 
+
+        } else {
+            intentos++;
+            alert('intento' + ' ' + intentos + ' ' +  'fallido. Quedan' + ' ' + (3 - intentos) + ' ' + 'intentos');
+
+
+        }
+    }
+
+    alert("Demasiados intentos fallidos. El sistema se cerrará.");
+    return false; 
+}
+
+iniciarSesion();
+
+
